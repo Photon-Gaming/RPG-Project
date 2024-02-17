816ac7fd2f083fa23e66bc5f53da4aa0ffaae8e5
TollyH <ptolemyhill@sky.com>
2024-02-17 22:33:57 +0000

    Initialise project templates

---
+++ RPGGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RPGGame
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            using RPGGame game = new();
+            game.Run();
+        }
+    }
+}
