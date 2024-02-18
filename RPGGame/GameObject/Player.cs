3371c4c79e74a445df09c98265b85cd80d7fc2d2
TollyH <ptolemyhill@sky.com>
2024-02-18 22:38:10 +0000

    Start Entity, Player, and Room classes

---
+++ RPGGame/GameObject/Player.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RPGGame.GameObject
+{
+    public class Player(Room currentRoom) : Entity(new EntityFile(Vector2.Zero, Vector2.One))
+    {
+        public Room CurrentRoom { get; private set; } = currentRoom;
+    }
+}
---
+++ RPGGame/GameObject/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace RPGGame.GameObject
+{
+    [Serializable]
+    public record RoomFile(
+        string[,] TileMap,
+        bool[,] CollisionMap,
+        Point Size
+    );
+
+    public class Room(RoomFile roomFile)
+    {
+        public RoomFile FileData { get; } = roomFile;
+    }
+}
