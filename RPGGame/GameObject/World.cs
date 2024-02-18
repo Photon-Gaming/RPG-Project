27a160802a4300c007c78b287296b0a642300a3c
TollyH <ptolemyhill@sky.com>
2024-02-18 23:57:19 +0000

    Add World class

---
+++ RPGGame/GameObject/World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace RPGGame.GameObject
+{
+    [Serializable]
+    public record WorldFile(
+        (string RoomName, Vector2 MapPosition)[] RoomPositions,
+        string DefaultRoomName
+    );
+
+    public class World(WorldFile worldFile)
+    {
+        public WorldFile FileData { get; } = worldFile;
+    }
+}
