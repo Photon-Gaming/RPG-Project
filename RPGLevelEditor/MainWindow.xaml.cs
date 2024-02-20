3ed1e48404d07c51c09d547cbad1d6823153a713
TollyH <ptolemyhill@sky.com>
2024-02-20 22:53:07 +0000

    Implement texture path configuration in editor

--- RPGLevelEditor/MainWindow.xaml.cs
+++ RPGLevelEditor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace RPGLevelEditor
 {
@@ -8,9 +10,38 @@ namespace RPGLevelEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const string ConfigFileName = "editor_config.json";
+
+        public Config EditorConfig { get; set; }
+
+        private readonly string configPath = Path.Join(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists(configPath))
+            {
+                try
+                {
+                    Config? deserialized = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
+                    if (deserialized is not null)
+                    {
+                        EditorConfig = deserialized;
+                    }
+                }
+                catch
+                {
+                    _ = MessageBox.Show(this, "Config file could not be read. Default settings will be used.",
+                        "Config File Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            if (EditorConfig is null)
+            {
+                EditorConfig = new Config();
+                _ = MessageBox.Show(this, "New config file created. Please configure the paths to the game resources.",
+                    "First run", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
 
         private void OpenRoomItem_OnClick(object sender, RoutedEventArgs e)
@@ -24,6 +55,23 @@ namespace RPGLevelEditor
             {
                 new RoomEditor(dialog.FileName, this).Show();
             }
+        }
+
+        private void TexturePathItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog dialog = new()
+            {
+                ValidateNames = true
+            };
+            if (dialog.ShowDialog() ?? false)
+            {
+                EditorConfig.TextureFolderPath = dialog.FolderName;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            File.WriteAllText(ConfigFileName, JsonConvert.SerializeObject(EditorConfig, Formatting.Indented));
         }
     }
 }
